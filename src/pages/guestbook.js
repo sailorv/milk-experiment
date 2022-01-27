159891d94c5158289299aec10b5fdf946d00a065
sailorv <sailorv@gmail.com>
2022-01-26 19:56:17 -0600

    Edits to guestbook

--- src/pages/guestbook.js
+++ src/pages/guestbook.js
@@ -18,12 +18,12 @@ const GuestbookPage = () => {
         <div className="col-wrapper">
             <div className="col-2">
                 <iframe src="https://live-milk-experiment.pantheonsite.io/guestbook/" style={{
-                    height: `400px`,
+                    height: `460px`,
                     border: `0`,
                     width: `100%`,
                     overflow: `hidden`,
                 }}
-                scrolling="no"
+                // scrolling="no"
                 ></iframe>
             </div>
 
