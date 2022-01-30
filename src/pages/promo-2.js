eba233e35beca5a2265a096127178a67a9deedbc
sailorv <sailorv@gmail.com>
2022-01-29 18:06:45 -0600

    promo pages added

---
+++ src/pages/promo-2.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+const BytesPage = () => {
+
+    return (
+<div style={{height: `100vh`, display: `flex`, alignItems: `center`, textAlign: `center`, width: `100%`}}>
+  <section id="page-header" style={{textAlign: `center`, width: `100%`}}>
+    <div id="page-title">
+      www.milk-experiment.net
+    </div><span className="cursor"></span>
+  </section>
+</div>
+    )
+}
+
+export default BytesPage
