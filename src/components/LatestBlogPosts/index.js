b164871f81451b28c6d9e9aad4e96a81d7e8c599
sailorv <sailorv@gmail.com>
2022-10-10 09:45:15 -0500

    Blog post formatting: hr widths, showing more blog posts on homepage

--- src/components/LatestBlogPosts/index.js
+++ src/components/LatestBlogPosts/index.js
@@ -9,7 +9,7 @@ const LatestBlogPosts = () => {
   return (
     <div className="posts-wrapper">
       <div className="excerpt-posts">
-        {new Array(3).fill("").map((element, i) => (
+        {new Array(4).fill("").map((element, i) => (
           <ExcerptPost
             key={i}
             title={post.allWpPost.edges[`${i + 2}`].node.title}
